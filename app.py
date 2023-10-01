d25bd11e9128b97c019648ca28937ecec09033ab
Muhammad Talha <muhammadtalhaawan801@gmail.com>
2023-10-01 18:00:35 +0500

    till model traning and making model pickle file

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig
 from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.components.model_tranier import ModelTrainer
+from src.mlproject.components.model_tranier import ModelTrainerConfig
 
 import sys
 
@@ -17,7 +19,12 @@ if __name__=="__main__":
         
         # data_transformation_config=DataIngestionConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+        # model_trainer_config=ModelTrainerConfig()
+        model_trainer=ModelTrainer()
+        r2_square=model_trainer.initiate_model_trainer(train_arr,test_arr)
+        print(f"\n\nr2_square:{r2_square} \n")
 
 
 
